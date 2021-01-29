8c4b68e789e43458d43557d7d28391396f69dde1
Anmora96 <anmora.2396@gmail.com>
2021-01-29 05:04:12 +0000

    Generador de cartas con cambio de color terminado

--- card.js
+++ card.js
@@ -4,18 +4,28 @@
 
 /*Variables que cotendran los simbolos*/
 let cards = ['♥', '♣', '♦', '♠'];
+/*            0    1    2    3*/
 let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-function displayCard() {
+function displayCard(){
     let singleCard = cards[Math.floor(Math.random() * cards.length)];
     let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-    let showCard = `${singleNumber}`;
-    let Icon1 = `${singleCard}`;
-
-    let SymbolHead = document.querySelector('#SymbolHead');
-    let SymbolFooter = document.querySelector('#SymbolFooter');
-    document.querySelector("#SymbolBody").innerHTML = showCard;
-    document.querySelector("#SymbolHead").innerHTML = Icon1;
-    document.querySelector("#SymbolFooter").innerHTML = Icon1;
     
+    /*se llaman las variables para los id*/
+    let SymbolHead = document.querySelector('#SymbolHead');
+    console.log("esto aparece: " + singleCard);
+    let SymbolFooter = document.querySelector('#SymbolFooter');
+    /*Se crean en el HTML*/
+    if( singleCard == '♥' || singleCard == '♦'){
+        document.querySelector("#SymbolHead").style.color = "red";
+        document.querySelector("#SymbolFooter").style.color = "red";
+    }
+    if( singleCard == '♣' || singleCard == '♠'){
+        document.querySelector("#SymbolHead").style.color = "black";
+        document.querySelector("#SymbolFooter").style.color = "black";
+    }
+    
+    document.querySelector("#SymbolBody").innerHTML = singleNumber;
+    document.querySelector("#SymbolHead").innerHTML = singleCard;
+    document.querySelector("#SymbolFooter").innerHTML = singleCard;
 }
